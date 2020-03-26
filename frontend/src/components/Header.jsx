9479889aa4031d10ff15cf907bc83fb8f6ed6fb0
elin.jonsdottir <elin.jonsdottir@oracle.com>
2020-03-26 14:51:11 +0000

    init frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React  from 'react'
+const Header = () => {
+
+    return (
+        <>
+            <div className="navbar">
+                EUROVISION-CONTEXT 2020
+            </div>
+        </>
+    )
+}
+
+export default Header
