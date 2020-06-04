4b249a8dc4ea6191c0a1bbe06c069d5a199fd32f
ilmaruk <ruggero.ferretti@oracle.com>
2020-06-04 10:47:14 +0100

    Additional thank-you message.

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -9,6 +9,8 @@ const ThankYouView = () => {
         <>
             <div className="container">
                 <h1 className="title">Thank You</h1>
+                <div className="thankyou">You're all done. Now please sit down and relax;
+                    the final results will be shown shortly.</div>
             </div>
         </>
     );
