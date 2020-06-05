4ec7525cf6337bf02453dd93a3091157d0abbd65
elin.jonsdottir <elin.jonsdottir@oracle.com>
2020-06-05 09:20:15 +0000

    added a start button on results page

--- frontend/src/pages/ResultsView.jsx
+++ frontend/src/pages/ResultsView.jsx
@@ -3,15 +3,26 @@ import ResultsList from "../components/Results";
 
 const ResultsView = () => {
 
+    const [showResults, results] = useState(false);
+
     useEffect(() => {
         document.title = 'List of songs';
     }, []);
+
+    const handleClick = () => {
+        results(true);
+    }
 
     return (
         <>
             <div className="container">
                 <h1 className="title">Results</h1>
-                <ResultsList/>
+                <div className="startButton">
+                    <button className={`button is-large ${showResults ? 'hideButton' : 'showButton'}`} onClick={handleClick}>START</button>
+                </div>
+                {
+                    showResults && <ResultsList/>
+                }
             </div>
         </>
     );
